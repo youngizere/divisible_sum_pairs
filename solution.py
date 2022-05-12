10b07ebd491086f4d12fc5b5141ba91fcf662c3e
youngizere <Young.Izere@strathmore.edu>
2022-05-12 16:59:14 +0300

    Solution refinement

--- solution.py
+++ solution.py
@@ -1,9 +1,12 @@
 def divisible_sum_pairs(input_int_array, k):
     count = 0
+    # Each element in the array is paired with each other element in the array in order 
+    # to computer the pair_sum as below implemented
     for i in input_int_array:
         for j in input_int_array:
-            if i < j:
+            if input_int_array.index(i) < input_int_array.index(j):
                 pair_sum = i+j
+                # if the remainder of pair_sum/k is zero then pair_sum is divisible by k
                 if pair_sum%k == 0:
                     count = count+1
     return count
